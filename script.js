b701aa048f109f0760a66c7145b28de156c286f3
Code Traversal <122677596+CodeTraversal@users.noreply.github.com>
2023-05-17 19:11:45 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
+let popup = document.querySelector('.popup');
 
-let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
+let openModal = () => {
+    popup.classList.add('popup_open');
+}
 
-        else if(e.target.innerHTML == 'AC'){
-            string = "";
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
-            input.value = string;
-        }
-        else{
-            string += e.target.innerHTML;
-            input.value = string;
-        }
-        
-    })
-})
+let closeModal = () =>{
+    popup.classList.remove('popup_open');
+}
